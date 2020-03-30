76d2ae2a8d55cdcf70e632663a47ca0fff1648fc
Klamanta <55380986+Klamanta@users.noreply.github.com>
2020-03-30 11:53:41 +0200

    Initial commit

---
+++ Footgolf/Footgolf/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Footgolf
+{
+    class Program
+    {
+        static List<Versenyzok> versenyzok;
+        static void Main(string[] args)
+        {
+            Beolvas();
+            Fel01();
+            Fel02(false);
+            Fel04(false);
+            //Fel05(true);
+            Console.ReadKey();
+        }
+
+        /*private static void Fel05(bool gender)
+        {
+        
+            var sw = new StreamWriter(@"ferfi.txt",false, Encoding.UTF8);
+            var a = versenyzok.Select(f => f.Nev).Where(f=> f.Kategoria == gender);
+            foreach (var v in versenyzok)
+            {
+                sw.WriteLine(a + ";" + );
+            }
+        }*/
+
+        private static void Fel04(bool gender)
+        {
+            var a = versenyzok.Where(f => f.Kategoria == gender).Max(f => f.Osszpontszam);
+            Console.WriteLine($"Feladat 6: \n\tNév: {a.Nev}\n\tEgyesület: {a.Egyesulet}\n\tÖsszpont: {a.Osszpontszam}");
+        }
+
+        private static void Fel02(bool gender)
+        {
+            var a = versenyzok.Count(f => f.Kategoria == gender);
+            var b = versenyzok.Count;
+            double szazalek = ((double)a / (double)b) * 100; 
+            Console.WriteLine($"Feladat 4: {Math.Round(szazalek,2)}");
+        }
+
+        private static void Fel01()
+        {
+            Console.WriteLine($"Fealadat 3: {versenyzok.Count}");
+        }
+
+        private static void Beolvas()
+        {
+            versenyzok = new List<Versenyzok>();
+            var sr = new StreamReader(@"doga.txt", Encoding.UTF8);
+            while (!sr.EndOfStream)
+            {
+                versenyzok.Add(new Versenyzok(sr.ReadLine()));
+
+            }
+            sr.Close();
+        }
+    }
+}
---
+++ Footgolf/Footgolf/Versenyzok.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Footgolf
+{
+    class Versenyzok
+    {
+        public string Nev { get; set; }
+        public bool Kategoria { get; set; }
+        public string Egyesulet { get; set; }
+        public int[] Helyezes = new int[8];
+        public int Osszpontszam => Fel03(Helyezes);
+
+        public Versenyzok(string sor)
+        {
+            string[] tmp = sor.Split(';');
+
+            this.Nev = tmp[0];
+            this.Kategoria = tmp[1] == "Felnott ferfi";
+            this.Egyesulet = tmp[2];
+            this.Helyezes[0] = int.Parse(tmp[3]);
+            this.Helyezes[1] = int.Parse(tmp[4]);
+            this.Helyezes[2] = int.Parse(tmp[5]);
+            this.Helyezes[3] = int.Parse(tmp[6]);
+            this.Helyezes[4] = int.Parse(tmp[7]);
+            this.Helyezes[5] = int.Parse(tmp[8]);
+            this.Helyezes[6] = int.Parse(tmp[9]);
+            this.Helyezes[7] = int.Parse(tmp[10]);  
+        }
+        private int Fel03(int[] Helyezes)
+        {
+            Array.Sort(Helyezes);
+            int osszpont = 0;
+            for (int i = 0; i < Helyezes.Length; i++)
+            {
+                if (Helyezes[i] != 0 && i < 2) Helyezes[i] = 10;
+                osszpont += Helyezes[i];
+            }
+            return osszpont;
+        }
+    }
+}
